ddab59d4081139b71467e8ddc1bdb4bef25d67fd
민동현 <mindonghyeon890@gmail.com>
2019-09-22 15:24:52 +0900

    Initial commit

---
+++ README .md
@@ -0,0 +1,6 @@
+## 👍 Collaborator
+
+#### 👩🏼‍💻[Seohyun Cho](https://github.com/S2eo)
+
+#### 👨🏻‍💻[Donghyeon Min](https://github.com/DaeAkin)
+
---
+++ peachmarket-login/src/main/java/peachMarketLoginApplication.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class peachMarketLoginApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(peachMarketLoginApplication.class, args);
+    }
+}
